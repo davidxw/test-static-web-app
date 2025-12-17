2e085ce1a83617057beffd6ad21c16e943961197
David Watson <dwatson@microsoft.com>
2025-12-17 11:49:57 +1100

    update API message

--- api/src/functions/managedHttpTrigger.ts
+++ api/src/functions/managedHttpTrigger.ts
@@ -3,7 +3,7 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 export async function managedHttpTrigger(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    const name = request.query.get('name') || await request.text() || 'from the API';
 
     return { body: `Hello, ${name}!` };
 };
